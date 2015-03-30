f21f803753b6fac0c8612f8f050120f0dfdbab85
Raúl Jornet <rjornetc@openmailbox.org>
2015-03-30 21:15:20 +0200

    Added two characters emojis

--- .xmlgenerator.py
+++ .xmlgenerator.py
@@ -5,7 +5,7 @@ from os import listdir, path
 
 
 
-emoji_path = path.join(path.curdir,'emojione')
+emoji_path = path.join(path.curdir,'emojione','non-added')
 
 print('<?xml version=\'1.0\'?>\n' +
       '<messaging-emoticon-map>')
@@ -13,8 +13,9 @@ print('<?xml version=\'1.0\'?>\n' +
 for filename in listdir(emoji_path):
     if filename != 'non-added' and filename != 'emoticons.xml':
         code = filename.split('.')[0]
+        codelist = code.split('-')
         print('    <emoticon file="' + code + '" >\n' +
-              '        <string>' + unichr(int(code,16)) + '</string>\n' +
+              '        <string>' + unichr(int(codelist[0],16)) + unichr(int(codelist[1],16)) + '</string>\n' +
               '    </emoticon>')
 
 print('</messaging-emoticon-map>')
